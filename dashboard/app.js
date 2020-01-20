af5afeaaed19107abec2f99f44e1f24ecd58a1d3
aakashDeveloper <ahanda205@gmail.com>
2020-01-20 17:19:40 +0000

    a

--- dashboard/app.js
+++ dashboard/app.js
@@ -11,6 +11,10 @@ var col_name="janUser"
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.get('/',(req,res) => {
+    res.send("Api is running");
+});
+
 // Add useer to DB
 app.post('/addUser',(req,res) => {
     db.collection(col_name)
@@ -24,11 +28,29 @@ app.post('/addUser',(req,res) => {
 });
 
 // Get User from DB
-app.get('/user',(req,res) => {
+/*app.get('/user',(req,res) => {
     db.collection(col_name).find().toArray((err,result) => {
         if(err) throw err;
         res.status(200).send(result)
     })
+});*/
+
+app.get('/user',(req,res) => {
+    var query = {};
+    if(req.query.id && req.query.name){
+        query = {name:req.query.name,id:parseInt(req.query.id),actin:true}
+    }else if(req.query.id){
+        query = {"id":parseInt(req.query.id),actin:true}
+    } else if(req.query.name){
+        query = {"name":req.query.name,actin:true}
+    } else{
+        query ={actin:true}
+    }
+
+    db.collection(col_name).find(query).toArray((err,result) => {
+        if(err) throw err;
+        res.status(200).send(result)
+    });
 });
 
 app.put('/updateUser',(req,res) => {
@@ -57,14 +79,23 @@ app.delete('/deleteUser',(req,res) => {
     }) 
 })
 
-app.put('/deleteUsr',(req,res) => {
-    db.collection(col_name).findOneAndDelete({
-        "id":req.body.id
-    },(err,result) => {
-        if(err) throw err;
-        res.send(result)
-    }) 
-})
+app.put('/softDelete',(req,res) => {
+    db.collection(col_name)
+        .findOneAndUpdate({'id': req.body.id},{
+            $set:{
+                id:req.body.id,
+                name:req.body.name,
+                city:req.body.city,
+                phone:req.body.phone,
+                actin:false
+            }
+        },{
+            upsert:true
+        },(err,result) => {
+            if(err) throw err;
+            res.send(result)
+        })
+});
 
 
 
